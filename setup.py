6dd0c2d07e7cbebd153ca9a3d32542a105e4ca93
David Buxton <david@dbuxton.com>
2024-09-25 09:06:48 +0100

    no pyc!

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
@@ -8,19 +11,13 @@ setup(
     version="0.1.0",
     author="David Buxton",
     author_email="david@dbuxton.com",
-    description="A Django extension for translating PO files using OpenAI",
+    description="A Django app for translating PO files using OpenAI",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/django-po-translator",
+    url="https://github.com/dbuxton/django-po-translator",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        "Django>=4.2",
-        "polib",
-        "openai",
-        "pytest",
-        "pytest-django",
-    ],
+    install_requires=requirements,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
